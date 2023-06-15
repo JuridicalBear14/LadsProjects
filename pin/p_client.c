efa784807dc87a2f704170593cf8a020af02d64c
JuridicalBear14 <throwawayemail2223@gmail.com>
2023-06-15 14:36:04 -0700

    Added port option

--- pin/p_client.c
+++ pin/p_client.c
@@ -9,7 +9,14 @@
 
 #define PORT 5555
 
-int main(void) {
+int main(int argc, char** argv) {
+    char* port = "127.0.0.1";
+
+    // Port given
+    if (argc > 1) {
+        port = argv[1];
+    }
+
     int status, valread, client_fd;
     struct sockaddr_in serv_addr;
     char* hello = "Hello from client";
@@ -18,30 +25,30 @@ int main(void) {
         printf("\n Socket creation error \n");
         return -1;
     }
-  
+
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-  
+
     // Convert IPv4 and IPv6 addresses from text to binary
     // form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, port, &serv_addr.sin_addr) <= 0) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
-  
+
     // Connect to server
     if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
         printf("\nConnection Failed \n");
         return -1;
     }
-    
+
     while (fgets(buffer, 1024, stdin) != NULL) {
         send(client_fd, buffer, strlen(buffer), 0);
         memset(buffer, 0,  strlen(buffer));
     }
-    
+
     //send(client_fd, hello, strlen(hello), 0);
-  
+
     // closing the connected socket
     close(client_fd);
     return 0;
